d4763bc6dc322332491032dd73939ae95d61e2f4
Dinesh Tyagi <dinesh777tyagi@gmail.com>
2020-10-22 15:10:56 +0530

    second commit digital_media

---
+++ build/precache-manifest.493770080ea54d0e7b973fb7f5779808.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c5d40598b345497f90a794268dfbd2",
+    "url": "/digitalmedia/index.html"
+  },
+  {
+    "revision": "fde10c8d0fff7390dc5d",
+    "url": "/digitalmedia/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "a69d789a8f9052927427",
+    "url": "/digitalmedia/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "fde10c8d0fff7390dc5d",
+    "url": "/digitalmedia/static/js/2.dc6a8ccf.chunk.js"
+  },
+  {
+    "revision": "9964024f48f40a411dca3b378fbfb349",
+    "url": "/digitalmedia/static/js/2.dc6a8ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69d789a8f9052927427",
+    "url": "/digitalmedia/static/js/main.1e506e72.chunk.js"
+  },
+  {
+    "revision": "022f346cb83c534304c0",
+    "url": "/digitalmedia/static/js/runtime-main.ac1e9305.js"
+  },
+  {
+    "revision": "e07b0975116d9c9b918bcb36f6223a5f",
+    "url": "/digitalmedia/static/media/1.e07b0975.jpg"
+  },
+  {
+    "revision": "a74f4a4997e4ac8c15a63e97fa25192b",
+    "url": "/digitalmedia/static/media/11.a74f4a49.jpg"
+  },
+  {
+    "revision": "687afc63c310381368561084ccfb8536",
+    "url": "/digitalmedia/static/media/12.687afc63.jpg"
+  },
+  {
+    "revision": "f885bb1d69ef9364e5c5a78fe0bc0861",
+    "url": "/digitalmedia/static/media/3.f885bb1d.jpg"
+  },
+  {
+    "revision": "ca8a1372e1523bf50a1efd3248d942b0",
+    "url": "/digitalmedia/static/media/4.ca8a1372.webp"
+  },
+  {
+    "revision": "ebf9cb4d253a57db6eadbe4df9398afa",
+    "url": "/digitalmedia/static/media/about.ebf9cb4d.jpg"
+  },
+  {
+    "revision": "0cf063a5dfcd4d66211e431ac5bdfb23",
+    "url": "/digitalmedia/static/media/app.0cf063a5.jpg"
+  },
+  {
+    "revision": "154aa89d7dbf48c60b18b632036d675e",
+    "url": "/digitalmedia/static/media/cloud.154aa89d.jpg"
+  },
+  {
+    "revision": "552b441962514456572015e97bc666c1",
+    "url": "/digitalmedia/static/media/mobile.552b4419.jpg"
+  }
+]);
